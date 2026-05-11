a9719413211d5a67ad8c937116ae692ef885bd3a
a <lounis03@gmail.com>
2026-05-11 08:47:10 +0100

    Refactor SerieDetail screen to improve code readability and add back navigation button

--- src/screens/SerieDetail.js
+++ src/screens/SerieDetail.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Caption, Divider, Menu, Subheading} from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
+import { Button, Caption, Divider, Icon, Menu, Subheading, IconButton } from 'react-native-paper';
 
 import {
   FlatList,
@@ -9,12 +9,12 @@ import {
   View,
 } from 'react-native';
 
-import {moviedb} from '../movieDb';
+import { moviedb } from '../movieDb';
 
 import useMainState from '../state/Main';
 
-const SerieDetails = ({navigation, route}) => {
-  const {serie} = route.params;
+const SerieDetails = ({ navigation, route }) => {
+  const { serie } = route.params;
   const [episodes, setEpisodes] = useState([]);
   const [season, setSeason] = useState(serie?.season || 1);
   const [episode, setEpisode] = useState(serie?.episode || 1);
@@ -32,7 +32,7 @@ const SerieDetails = ({navigation, route}) => {
     setSeasons([]);
     // don't mutate route params; compute image separately
     moviedb
-      .tvInfo({id: serie.id, append_to_response: 'seasons'})
+      .tvInfo({ id: serie.id, append_to_response: 'seasons' })
       .then(res => {
         setSeasons(res.seasons.length || 0);
         // prefill serie.image if it wasn't provided
@@ -40,7 +40,7 @@ const SerieDetails = ({navigation, route}) => {
           serie.image = `https://image.tmdb.org/t/p/w500/${res.poster_path}`;
         }
       })
-      .catch(() => {})
+      .catch(() => { })
       .finally(() => setLoading(false));
   }, [serie?.id]);
 
@@ -50,7 +50,7 @@ const SerieDetails = ({navigation, route}) => {
     setEpisodes([]);
     setLoading(true);
     moviedb
-      .seasonInfo({id: serie.id, season_number: season, append_to_response: 'episodes'})
+      .seasonInfo({ id: serie.id, season_number: season, append_to_response: 'episodes' })
       .then(res => {
         setEpisodes(res.episodes || []);
       })
@@ -66,7 +66,7 @@ const SerieDetails = ({navigation, route}) => {
     setEpisodes([]);
   }, [serie?.id]);
 
-  const EpisodeItem = ({episode, s, navigation}) => {
+  const EpisodeItem = ({ episode, s, navigation }) => {
     const state = useMainState();
 
     return (
@@ -106,10 +106,10 @@ const SerieDetails = ({navigation, route}) => {
             source={{
               uri: `https://image.tmdb.org/t/p/w500/${episode.still_path}`,
             }}
-            style={{width: 100, height: 100, borderRadius: 10}}
+            style={{ width: 100, height: 100, borderRadius: 10 }}
           />
-          <Caption style={{padding: 10}}>{episode.episode_number}</Caption>
-          <Caption style={{padding: 10}}>{episode.name}</Caption>
+          <Caption style={{ padding: 10 }}>{episode.episode_number}</Caption>
+          <Caption style={{ padding: 10 }}>{episode.name}</Caption>
         </View>
         <Divider />
       </TouchableOpacity>
@@ -120,8 +120,21 @@ const SerieDetails = ({navigation, route}) => {
 
   return (
     <ScrollView>
-      <Image source={{uri: serie.image}} style={{width: '100%', height: 200}} />
-      <Subheading style={{padding: 10}}>
+      <Image source={{ uri: serie.image }} style={{ width: '100%', height: 200 }} />
+
+      <IconButton
+        mode="contained-tonal"
+        onPress={() => {
+          if (route?.params?.fromSearch) {
+            navigation.navigate('Search');
+          } else {
+            navigation.goBack();
+          }
+        }}
+        style={{ position: 'absolute', top: 10, left: 10, zIndex: 1 }}
+        icon="arrow-left"
+      />
+      <Subheading style={{ padding: 10 }}>
         {serie.title} ({serie.year})
       </Subheading>
       {continueWatching ? (
@@ -145,7 +158,7 @@ const SerieDetails = ({navigation, route}) => {
         }}
         anchor={
           <Button
-            style={{width: 200}}
+            style={{ width: 200 }}
             mode="text"
             onPress={() => {
               setMenuVisible(true);
@@ -154,8 +167,8 @@ const SerieDetails = ({navigation, route}) => {
           </Button>
         }>
         <FlatList
-          data={Array.from({length: seasons}, (_, i) => i + 1)}
-          renderItem={({item}) => (
+          data={Array.from({ length: seasons }, (_, i) => i + 1)}
+          renderItem={({ item }) => (
             <Menu.Item
               onPress={() => {
                 setSeason(item);
@@ -169,7 +182,7 @@ const SerieDetails = ({navigation, route}) => {
       </Menu>
       <FlatList
         data={episodes}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <EpisodeItem episode={item} s={serie} navigation={navigation} />
         )}
         keyExtractor={item => item.id.toString()}
